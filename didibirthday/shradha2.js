fc2bfcb524784dddef72944f8fc2c3f51317843e
codeit-dotcom <72062506+codeit-dotcom@users.noreply.github.com>
2020-11-05 11:03:51 +0530

    Add files via upload

---
+++ didibirthday/shradha2.js
@@ -0,0 +1,19 @@
+$(".secret a").on("click",function(){
+	alert("Oh you got it... NOW take a deep breathe and wear a smile... Hmmm now ur are ready for a Short Movie.... If using mobile phone view it in landscape mode...  It's SHOW TIME!!!  (Press Okay) ");
+});
+
+$(".best a").on("click",function(){
+	alert("Your device doesn't support this feature.");
+	alert("Anyway, I will try to help you. Press OK and follow the instructions.");
+	alert("For best view, open the link in your pc and set your browser window width to 60% of your pc's screen width. Goodluck!");
+});
+
+alert("Every floating text has some secret message (instructions), make sure you are reading it carefully...");
+
+
+$(".button button").on("click",function(){
+	alert("I have a surprise for u, hidden on this page itself, behind a 'secret message', click on it and take ur gift.                                                        Isii page mein hai, Dundho! Dundho! *                                                                                Hint: aasmaaan ke uchaiyon mein dekho...");
+});
+
+
+//<div class="tenor-gif-embed" data-postid="4480424" data-share-method="host" data-width="100%" data-aspect-ratio="1.0"><a href="https://tenor.com/view/cake-gif-4480424">Covered In Chocolate - Cake GIF</a> from <a href="https://tenor.com/search/cake-gifs">Cake GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
